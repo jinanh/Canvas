4926d117c4ad66e3641f2be89be1ec242bcabcd6
jinanh <jhuang27@mail.sfsu.edu>
2018-10-08 18:44:15 -0700

    Initial Commit

---
+++ Canvas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Canvas
+//
+//  Created by Jinan Huang on 10/8/18.
+//  Copyright © 2018 Jinan Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
